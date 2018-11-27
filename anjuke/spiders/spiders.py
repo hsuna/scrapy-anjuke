45c2fcdb8edba40ae3f511323220bf84ce89c448
hsuna <me@hsuna.com>
2018-11-27 20:13:00 +0800

    update

--- anjuke/spiders/spiders.py
+++ anjuke/spiders/spiders.py
@@ -7,8 +7,7 @@ from pymongo import MongoClient
 import scrapy
 from scrapy.conf import settings
 from scrapy.selector import Selector
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider
 from anjuke.items import PageItem, HouseItem
 
 
